910ce00877bd2e4f754d9bb1e99690f98faca4f9
hypno-coder <davidov.artur.job@gmail.com>
2022-08-21 13:01:56 +0300

    apiArticles complete

--- backend/server/article/urls.py
+++ backend/server/article/urls.py
@@ -2,13 +2,14 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import UserAccessViewSet, ArticleTextViewSet, BonusViewSet, ExtraViewSet
+from .views import ArticleViewSet, UserAccessViewSet, ArticleTextViewSet, BonusViewSet, ExtraViewSet
 
 
 app_name = 'article'
 
 router = DefaultRouter()
 
+router.register('articles', ArticleViewSet, basename='articles')
 router.register('user-access', UserAccessViewSet, basename='user-access')
 router.register('article-text', ArticleTextViewSet, basename='article-text')
 router.register('extra', ExtraViewSet, basename='extra')
