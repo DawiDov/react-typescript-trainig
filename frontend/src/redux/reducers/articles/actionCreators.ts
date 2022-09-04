00854fc0f91a9b7172f8fa121ec09b1d5984ca0a
Artur Davidov <davidov.artur.job@gmail.com>
2022-09-04 14:35:37 +0300

    Prep prod (#100)
    * prod chnges
    
    * prod db
    
    * prod change domaine
    
    * fix actions
    
    * remove pack.json

--- frontend/src/redux/reducers/articles/actionCreators.ts
+++ frontend/src/redux/reducers/articles/actionCreators.ts
@@ -79,7 +79,8 @@ const articlesActionCreators = {
   },
   updateInstructionAccess: async (instHandle: boolean) => {
     const token: string | null = localStorage.getItem('token')
-    const url: string = 'https://masterskaya-courses.ru/api/user-access/inst-access/'
+    const url: string =
+      'https://masterskaya-courses.ru/api/user-access/inst-access/'
 
     const requestHeaders = {
       'Authorization': `Token ${token}`, // eslint-disable-line
