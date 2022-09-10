f4f04319235dd16c1369eb3883a7cc1f357d9999
hypno-coder <davidov.artur.job@gmail.com>
2022-09-10 16:34:22 +0300

    fix redirect complete

--- frontend/src/components/layout/Layout/index.tsx
+++ frontend/src/components/layout/Layout/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation, Navigate } from 'react-router-dom'
 import Header from 'components/layout/Header'
 import { Box } from '@mui/system'
 
-const Layout = () => (
-  <Box
-    sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-    }}>
-    <Header />
-    <Outlet />
-  </Box>
-)
+const Layout = () => {
+  const location = useLocation()
+
+  if (location.pathname === '/') {
+    return <Navigate to="/login" replace />
+  }
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+      }}>
+      <Header />
+      <Outlet />
+    </Box>
+  )
+}
 
 export default Layout
